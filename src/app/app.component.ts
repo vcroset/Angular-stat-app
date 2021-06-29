d1efd6c8fc6bec6d1a22b51f6f74f8b2ace174ed
BadValou <valentin.croset@gmail.com>
2021-06-29 11:57:30 +0200

    Exercice 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,18 +7,33 @@ import {Statistique} from './models/statistique';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    public statistique1: Statistique;
-    public statistique2: Statistique;
+    // public statistique1: Statistique;
+    // public statistique2: Statistique;
     public listStatistiques: Statistique[];
 
     constructor() {
-        this.statistique1 = new Statistique('1', 'Test 1', '20', 'SUCCESS');
-        this.statistique2 = new Statistique('2', 'Test 2', '10', 'SUCCESS');
-        this.listStatistiques = [this.statistique1, this.statistique2];
+        // this.statistique1 = new Statistique('1', 'Test 1', '20', 'SUCCESS');
+        // this.statistique2 = new Statistique('2', 'Test 2', '10', 'SUCCESS');
+        // this.listStatistiques = [this.statistique1, this.statistique2];
 
-        setTimeout(() => {
-            this.listStatistiques.push(new Statistique('3', 'Test 3', '30', 'SUCCESS'));
-        }, 5000);
+        // setTimeout(() => {
+            // this.listStatistiques.push(new Statistique('3', 'Test 3', '30', 'SUCCESS'));
+        // }, 5000);
+        this.listStatistiques = [];
+
+        fetch('https://stats.naminilamy.fr/')
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+              data.forEach((stat: { id: string; title: string; value: number; appreciation: string; }) => {
+                this.listStatistiques.push(new Statistique('' + stat.id, '' + stat.title,
+                    '' + stat.value, '' + stat.appreciation));
+              });
+            })
+            .catch((err) => {
+                console.error(err);
+            });
     }
 
 }
