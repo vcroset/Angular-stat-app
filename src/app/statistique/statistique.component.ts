e69b8852392beff7ce360745bdb119c2e88982f6
BadValou <valentin.croset@gmail.com>
2021-06-29 12:31:06 +0200

    Exercice 6

--- src/app/statistique/statistique.component.ts
+++ src/app/statistique/statistique.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostListener, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {Statistique} from '../models/statistique';
 
 @Component({
@@ -6,7 +6,7 @@ import {Statistique} from '../models/statistique';
     templateUrl: './statistique.component.html',
     styleUrls: ['./statistique.component.css']
 })
-export class StatistiqueComponent implements OnInit {
+export class StatistiqueComponent implements OnInit, OnDestroy {
 
     @Input()
     public statistiqueCom!: Statistique;
@@ -17,4 +17,19 @@ export class StatistiqueComponent implements OnInit {
     ngOnInit(): void {
     }
 
+    @HostListener('unloaded')
+    ngOnDestroy(): void {
+        console.log('item destroy');
+    }
+
+    delete(): void {
+        console.log(this.statistiqueCom.identifiant);
+        fetch('https://stats.naminilamy.fr/' + this.statistiqueCom.identifiant,
+            {method: 'DELETE'}).then(resp => {
+            if (resp.status === 200) {
+                this.ngOnDestroy();
+                console.log('api destroy success !!!');
+            }
+        });
+    }
 }
